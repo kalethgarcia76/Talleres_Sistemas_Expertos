a4bc0ddb15b4120c4461f82b80b919280ea2a233
kalethgarcia76 <kalethgarcia76@gmail.com>
2026-09-19 20:09:15 +0000

    talleres: creacion del  init del modulo 8

---
+++ modulo_8/perceptron_experto/__init__.py
@@ -0,0 +1,11 @@
+from .perceptron import funcion_escalon, perceptron
+from .compuertas_modulo import obtener_pesos, COMPUERTAS
+from .compuertas_case import obtener_pesos_case
+
+__all__ = [
+    "funcion_escalon",
+    "perceptron",
+    "obtener_pesos",
+    "COMPUERTAS",
+    "obtener_pesos_case",
+]
